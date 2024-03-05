034730cb2f8e6bfc113427aad82ba96ad812f7de
ichiro-ss <sei.swmr@gmail.com>
2024-03-05 14:02:06 +0900

    solved 104

---
+++ p104/main.go
@@ -0,0 +1,13 @@
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return int(math.Max(float64(maxDepth(root.Left)+1), float64(maxDepth(root.Right)+1)))
+}
