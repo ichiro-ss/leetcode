d21dddda163b1ae24c4907897c8e37cbacdb7937
ichiro-ss <sei.swmr@gmail.com>
2024-03-06 20:48:58 +0900

    solved p78

--- p78/main.go
+++ p78/main.go
@@ -9,7 +9,8 @@ import (
 
 func main() {
 	lines := []int{1, 2, 3}
-	ans := subsets(lines)
+	// ans := subsets(lines)
+	ans := subsets2(lines)
 	fmt.Println(ans)
 }
 
@@ -24,6 +25,25 @@ func subsets(nums []int) [][]int {
 		}
 		ans = append(ans, tmp)
 	}
+	return ans
+}
+
+func subsets2(nums []int) [][]int {
+	ans := make([][]int, 0)
+	cur := make([]int, 0)
+	var dfs func(i int)
+	dfs = func(i int) {
+		if i >= len(nums) {
+			ans = append(ans, append([]int{}, cur...))
+			fmt.Println(ans)
+			return
+		}
+		cur = append(cur, nums[i])
+		dfs(i + 1)
+		cur = cur[:len(cur)-1]
+		dfs(i + 1)
+	}
+	dfs(0)
 	return ans
 }
 
